14e74db71c5a315aff9d7cf4e0835a261b1959c6
Merja <merja@dicom.com>
2022-01-13 15:53:18 +0100

    Verknüpfung der Seiten und Einführen OFFISLogos

--- DICOMOFFIS/urls.py
+++ DICOMOFFIS/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from DICOMOFFIS import views
 
 urlpatterns =[
-   path("",views.index2, name =("index2"))
+   path("",views.index2, name =("index2")),
+   path('Allgemeines.html/', views.allgemeines, name= ('Allgemeines')),
+   path('DCMTK.html/', views.dcmtk, name = ('dcmtk') ),
+   path('Home.html/' , views.home, name = ('home')),
 ]
--- DICOMOFFIS/views.py
+++ DICOMOFFIS/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from DICOMOFFIS import views
 
 # Create your views here.
 def index2 (request):
     return render (request,'Website/index2.html');
+
+def allgemeines (request):
+    return render (request,'Website/Allgemeines.html')
+
+def dcmtk (request):
+    return render (request, 'Website/DCMTK.html')
+
+def home (request):
+    return render (request, 'Website/Home.html')
