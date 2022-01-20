7139982859ade1f9580f5a79ad289c0d16a0e658
Merja <merja@dicom.com>
2022-01-20 12:22:51 +0100

    Menüs und Header

--- DICOMOFFIS/urls.py
+++ DICOMOFFIS/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
+from django.shortcuts import redirect
+
 from DICOMOFFIS import views
 
 urlpatterns =[
    path("",views.index2, name =("index2")),
    path('Allgemeines/', views.allgemeines, name= ('Allgemeines')),
    path('DCMTK/', views.dcmtk, name = ('dcmtk') ),
-   path('Home/' , views.home, name = ('home')),
+   path('Home/' , lambda req: redirect('/')),
+   
 ]
